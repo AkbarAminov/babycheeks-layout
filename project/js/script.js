1b4f016e37970776bc249eeab45801b4130d4e5c
AkbarAminov <akbar.aminov.97@gmail.com>
2019-03-28 22:31:44 +0500

    added carousel to main page(icons, owl carousel), repair footer

---
+++ project/js/script.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  nav:true,
+  dots: false,
+  animateOut: 'fadeOut',
+  
+  navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>',
+          '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'
+      ], //we will be using font awesome icon here
+  responsive:{
+      0:{
+          items:1
+      },
+      900:{
+          item:3
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:4
+      }
+  }
+})
