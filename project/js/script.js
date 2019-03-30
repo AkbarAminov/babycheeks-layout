5f86f2403fe3e0861bd93bd1cba54f5905e79c2e
AkbarAminov <akbar.aminov.97@gmail.com>
2019-03-31 02:19:29 +0500

    Child Category Page -> added fixed sidebar, Product page -> added description part

--- project/js/script.js
+++ project/js/script.js
@@ -1,33 +1,34 @@
 $(document).ready(function () {
 
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  nav:true,
-  dots: false,
-  animateOut: 'fadeOut',
-  
-  navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>',
-          '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'
-      ], //we will be using font awesome icon here
-  responsive:{
-      0:{
-          items:1
-      },
-      900:{
-          item:3
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:4
-      }
-  }
-})
+  $('.owl-1').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    animateOut: 'fadeOut',
 
-$('.expand-button').on('click', function(){
+    navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'
+    ], //we will be using font awesome icon here
+    responsive: {
+      0: {
+        items: 1
+      },
+      900: {
+        item: 3
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+
+
+  $('.expand-button').on('click', function () {
     $('.special-text').toggleClass('-expanded');
-    
+
     if ($('.special-text').hasClass('-expanded')) {
       $('.expand-button').html('Свернуть');
     } else {
