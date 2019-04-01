d2e46c1636696414a1a48b412fb8d6d5f9d670c3
AkbarAminov <akbar.aminov.97@gmail.com>
2019-04-01 23:16:25 +0500

    Product Page -> added product description part

--- project/js/bulma-fixed-sidenav.js
+++ project/js/bulma-fixed-sidenav.js
@@ -1,4 +1,35 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function () {
+
+  // Cookies
+
+  var cookieBookModalName = 'bulma_closed_book_modal';
+  // var cookieBookModal = Cookies.getJSON(cookieBookModalName) || false;
+
+  // Sidebar links
+
+  var $categories = getAll('#categories .bd-category');
+
+  if ($categories.length > 0) {
+    $categories.forEach(function (el) {
+      var toggle_el = el.querySelector('.bd-category-toggle');
+
+      toggle_el.addEventListener('click', function (event) {
+        // closeCategories(el);
+        el.classList.toggle('is-active');
+      });
+    });
+  }
+
+  function closeCategories(current_el) {
+    $categories.forEach(function (el) {
+      if (current_el == el) {
+        return;
+      }
+      el.classList.remove('is-active');
+    });
+  }
 
   var anchors_ref_el = document.getElementById('anchorsReference');
   var anchors_el = document.getElementById('anchors');
@@ -65,6 +96,169 @@ document.addEventListener('DOMContentLoaded', function () {
       el.classList.remove('is-active');
     });
   }
+
+  // Meta links
+
+  var $metalinks = getAll('#meta a');
+
+  if ($metalinks.length > 0) {
+    $metalinks.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.preventDefault();
+        var target = $el.getAttribute('href');
+        var $target = document.getElementById(target.substring(1));
+        $target.scrollIntoView(true);
+        return false;
+      });
+    });
+  }
+
+  // Dropdowns
+
+  var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+
+  if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.stopPropagation();
+        $el.classList.toggle('is-active');
+      });
+    });
+
+    document.addEventListener('click', function (event) {
+      closeDropdowns();
+    });
+  }
+
+  function closeDropdowns() {
+    $dropdowns.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  // Toggles
+
+  var $burgers = getAll('.burger');
+
+  if ($burgers.length > 0) {
+    $burgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+
+  // Modals
+
+  var rootEl = document.documentElement;
+  var $modals = getAll('.modal');
+  var $modalButtons = getAll('.modal-button');
+  var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+
+  if ($modalButtons.length > 0) {
+    $modalButtons.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        openModal(target);
+      });
+    });
+  }
+
+  if ($modalCloses.length > 0) {
+    $modalCloses.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        closeModals();
+      });
+    });
+  }
+
+  function openModal(target) {
+    var $target = document.getElementById(target);
+    rootEl.classList.add('is-clipped');
+    $target.classList.add('is-active');
+  }
+
+  function closeModals() {
+    rootEl.classList.remove('is-clipped');
+    $modals.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  document.addEventListener('keydown', function (event) {
+    var e = event || window.event;
+    if (e.keyCode === 27) {
+      closeModals();
+      closeDropdowns();
+    }
+  });
+
+  // Clipboard
+
+  var $highlights = getAll('.highlight');
+  var itemsProcessed = 0;
+
+  if ($highlights.length > 0) {
+    $highlights.forEach(function ($el) {
+      var copyEl = '<button class="button is-small bd-copy">Copy</button>';
+      var expandEl = '<button class="button is-small bd-expand">Expand</button>';
+      $el.insertAdjacentHTML('beforeend', copyEl);
+
+      var $parent = $el.parentNode;
+      if ($parent && $parent.classList.contains('bd-is-more')) {
+        var showEl = '<button class="bd-show"><div><span class="icon"><i class="fas fa-code"></i></span> <strong>Show code</strong></div></button>';
+        $el.insertAdjacentHTML('beforeend', showEl);
+      } else if ($el.firstElementChild.scrollHeight > 480 && $el.firstElementChild.clientHeight <= 480) {
+        $el.insertAdjacentHTML('beforeend', expandEl);
+      }
+
+      itemsProcessed++;
+      if (itemsProcessed === $highlights.length) {
+        addHighlightControls();
+      }
+    });
+  }
+
+  function addHighlightControls() {
+    var $highlightButtons = getAll('.highlight .bd-copy, .highlight .bd-expand');
+
+    $highlightButtons.forEach(function ($el) {
+      $el.addEventListener('mouseenter', function () {
+        $el.parentNode.classList.add('bd-is-hovering');
+      });
+
+      $el.addEventListener('mouseleave', function () {
+        $el.parentNode.classList.remove('bd-is-hovering');
+      });
+    });
+
+    var $highlightExpands = getAll('.highlight .bd-expand');
+
+    $highlightExpands.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        $el.parentNode.firstElementChild.style.maxHeight = 'none';
+      });
+    });
+
+    var $highlightShows = getAll('.highlight .bd-show');
+
+    $highlightShows.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        $el.parentNode.parentNode.classList.remove('bd-is-more-clipped');
+      });
+    });
+  }
+
+  // setTimeout(function () {
+  //   new Clipboard('.bd-copy', {
+  //     target: function target(trigger) {
+  //       return trigger.previousElementSibling.firstElementChild;
+  //     }
+  //   });
+  // }, 100);
 
   // Functions
 
@@ -240,4 +434,20 @@ document.addEventListener('DOMContentLoaded', function () {
     ticking = true;
   });
 
+  // Utils
+
+  function removeFromArray(array, value) {
+    if (array.includes(value)) {
+      var value_index = array.indexOf(value);
+      array.splice(value_index, 1);
+    }
+
+    return array;
+  }
+
+  Array.prototype.diff = function (a) {
+    return this.filter(function (i) {
+      return a.indexOf(i) < 0;
+    });
+  };
 });
