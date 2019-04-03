ae1024f8d91e72ed65475b17859006ad97a045c1
AkbarAminov <akbar.aminov.97@gmail.com>
2019-04-03 20:02:56 +0500

    Product Page ->deleted owl carousel and  added swiper carousel

--- project/js/script.js
+++ project/js/script.js
@@ -1,31 +1,4 @@
 $(document).ready(function () {
-
-  $('.owl-1').owlCarousel({
-    loop: true,
-    nav: true,
-    dots: false,
-    animateOut: 'fadeOut',
-
-    navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>',
-      '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'
-    ], //we will be using font awesome icon here
-    responsive: {
-      0: {
-        items: 1
-      },
-      900: {
-        item: 3
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 4
-      }
-    }
-  })
-
-
   $('.expand-button').on('click', function () {
     $('.special-text').toggleClass('-expanded');
 
@@ -35,6 +8,4 @@ $(document).ready(function () {
       $('.expand-button').html('Читать Больше');
     }
   });
-
-
 })
